0c6a1e3a19aa17f8d0beef958310ddc5c976c164
Maquim4 <konohov.andreyka@gmail.com>
2021-10-12 22:01:00 +0300

    (Task#2):languages.
    added languages

--- Hello world/Source.cpp
+++ Hello world/Source.cpp
@@ -2,8 +2,31 @@
 using namespace std;
 int main()
 {
-	
+	setlocale(LC_ALL, "RU");
 	cout << " Hello, World\t\n\n\n\n\n\n";
+	cout << " Would you translate it?\n 1.Yes\n 2.No\n";
+	int choice12, choice;
+	cin>>choice12;
+	switch (choice12)
+	{
+	case 1: cout << " What language you need?\n 1.Russian\n 2.Kazakh\n 3.Chinese\n";
+		break;
+	case 2: cout << " Ok, have a nice day\n";
+		break;
+	}
+	cin >> choice;
+	if (choice12 != 2)
+	{
+       switch (choice)
+	   {
+	   case 1:cout << " Привет, Мир\n";
+		   break;
+	   case 2:cout << " Salem Alem\n";
+		   break;
+	   case 3:cout << " Ni2hao3,shi4jie4\n";
+		   break;
+	   }
+	}
 	
 
 }
